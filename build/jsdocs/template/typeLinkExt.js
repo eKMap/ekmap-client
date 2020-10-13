5328c74f76ccdd6b3f8e12e469d2bd0427c5e88a
duongnv1989 <nvduongcoltech@gmail.com>
2020-10-13 10:54:45 +0700

    Docs

--- build/jsdocs/template/typeLinkExt.js
+++ build/jsdocs/template/typeLinkExt.js
@@ -2,7 +2,6 @@ var olapi = "https://openlayers.org/en/v6.1.1/apidoc/";
 var lfapi = "https://leafletjs.com/reference-1.5.1.html";
 var mbglapi = "https://www.mapbox.com/mapbox-gl-js/api/";
 var mapv = "https://github.com/huiyan-fe/mapv/blob/master/src/";
-var classicapi="https://iclient.supermap.io/web/libs/iclient8c/apidoc/files/SuperMap"
 var geojsonapi = "https://geojson.org";
 var typeLinks = {
 
@@ -13,10 +12,6 @@ var typeLinks = {
     //mapv
     "Mapv.DataSet": mapv + 'data/DataSet.md',
     "Mapv.BaiduMapLayer": mapv + 'map/baidu-map/Layer.md',
-
-    //classic
-    "SuperMap.Layer":classicapi + '/Layer-js.html',
-    "SuperMap.Layer.Graph": classicapi + '/Layer/Theme/Graph-js.html#SuperMap.Layer.Graph',
     
 	//GeoJSON
     "GeoJSONObject": geojsonapi
